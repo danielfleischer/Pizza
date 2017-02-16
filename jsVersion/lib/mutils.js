f6018c42ecec35ded49ede9453a3a4c182b9f64a
thehobbit85 <thehobbit85@gmail.com>
2017-02-17 00:08:42 +0200

    Achivement UNLOCKED - O(N^2) PER CHILD PER GENERATION WHEN N IS THE LENGTH OF THE SOLUTION NOT THE PROBLEM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- jsVersion/lib/mutils.js
+++ jsVersion/lib/mutils.js
@@ -1,43 +1,40 @@
-const _ = require('lodash')
-
 let getRandomInt = ([min, max]) => Math.floor(Math.random() * (max - min + 1)) + min
 
 let area = (x1, y1, x2, y2) => Math.abs((x2 - x1 + 1) * (y2 - y1 + 1))
 
 let singleFit = (S) => S.reduce((r, s) => r + area(s[0][0], s[0][1], s[1][0], s[1][1]), 0)
 
-let flattenMatrix = ({matrix, x1, y1, x2, y2}) => {
+let isOverLap = (verifiedSlices, lx1, ly1, rx1, ry1) => {
+  if (!verifiedSlices.length) return false
+  for (let i = 0, len = verifiedSlices.length; i < len; i++) {
+    let slice = verifiedSlices[i]
+    if (!(lx1 > slice[1][0] || slice[0][0] > rx1)) return true
+    if (!(ly1 < slice[1][1] || slice[0][1] < ry1)) return true
+  }
+  return false
+}
+
+let sumArea = ({matrix, x1, y1, x2, y2}) => {
   let result = []
   for (let i = y1; i <= y2; i++) {
     for (let j = x1; j <= x2; j++) result.push(matrix[i][j])
   }
-  return result
-}
-
-let isOverlap = (part) => flattenMatrix(part).filter(item => !item).length > 0
-
-let sumArea = (part) => flattenMatrix(part).reduce((a, b) => a + b, 0)
-
-let insertSliceToMatrix = (matrix, x1, y1, x2, y2) => {
-  for (let i = y1; i <= y2; i++) {
-    for (let j = x1; j <= x2; j++) matrix[i][j] = 0
-  }
-  return matrix
+  return result.reduce((a, b) => a + b, 0)
 }
 
 let isSlicesValid = (slices, matrix, L, H) => {
-  let coverMatrix = _.cloneDeep(matrix)
+  let verifiedSlices = []
   for (let i = 0; i < slices.length; i++) {
     let [[x1, y1], [x2, y2]] = slices[i]
     let sliceArea = area(x1, y1, x2, y2)
     if (sliceArea > H) return false
     let areaSum = sumArea({matrix, x1, y1, x2, y2})
     if (areaSum < L - sliceArea || areaSum > sliceArea - L) return false
-    if (isOverlap({matrix: coverMatrix, x1, y1, x2, y2})) return false
-    coverMatrix = insertSliceToMatrix(coverMatrix, x1, y1, x2, y2)
+    if (isOverLap(verifiedSlices, x1, y1, x2, y2)) return false
+    verifiedSlices.push([[x1, y1], [x2, y2]])
   }
   return true
 }
 
-module.exports = { area, getRandomInt, isOverlap, isSlicesValid, singleFit }
+module.exports = { area, getRandomInt, isSlicesValid, singleFit }
 
--- jsVersion/test/test.js
+++ jsVersion/test/test.js
@@ -20,7 +20,7 @@ describe('testing on google samples', function () {
           let filePath = path.join(inputFolder, file)
           let resultsFileName = path.join(resultsdir, file.split('.')[0] + '-' + (new Date()).getTime() + '.json')
           config.POPULATIONSIZE = Math.pow(poolSizeBash, 2)
-          config.CREATIONRETRY = 1000
+          config.CREATIONRETRY = 2500
           console.time('evolution')
           let results = evolution(filePath, config)
           console.timeEnd('evolution')
