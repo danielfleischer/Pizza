017f1e04e46ad4a68f8edfc03c88533702658d09
Eliran Zach <eliran.zach@bitmaintech.com>
2017-02-16 14:07:53 +0200

    improvements to tests

--- jsVersion/test/test.js
+++ jsVersion/test/test.js
@@ -4,33 +4,33 @@ const fs = require('fs')
 const path = require('path')
 const resultsdir = path.join(__dirname, './results/')
 const inputFolder = path.join(__dirname, './inputs')
+const inputFileToJSON = require(path.join(__dirname, './../lib/inputFileToJSON'))
 
-let config = {
-  CREATIONRETRY: 30,
-  POPULATIONSIZE: 100,
-  MUTATIONRETRY: 100,
-  GENERATIONS: 50,
-  MUTATIONRATE: 5,
-  verbose: false
-}
+let config = Object.assign({RUNS: 1}, process.env)
+
 if (!fs.existsSync(resultsdir)) fs.mkdirSync(resultsdir)
 
 describe('testing on google samples', function () {
   this.timeout(0)
   it('should end', function (done) {
+    let poolSizeBash = config.POOLSIZEBASE
     fs.readdir(inputFolder, function (err, files) {
       if (err) throw err
-      files.forEach(function (file) {
-        let filePath = path.join(inputFolder, file)
-        let resultsFileName = path.join(resultsdir, file.split('.')[0] + '-' + (new Date()).getTime() + '.json')
-        // config.CREATIONRETRY = file === 'medium' ? 5 : 30
-        // config.POPULATIONSIZE = file === 'medium' ? 49 : 100
-        console.time('evolution')
-        let results = evolution(filePath, config)
-        console.timeEnd('evolution')
-        jf.writeFileSync(resultsFileName, results)
-        console.log('Finished with file', file)
-      })
+      for (let i = 0; i < config.RUNS; i++) {
+        files.forEach(function (file) {
+          let filePath = path.join(inputFolder, file)
+          let resultsFileName = path.join(resultsdir, file.split('.')[0] + '-' + (new Date()).getTime() + '.json')
+          config.POPULATIONSIZE = Math.pow(poolSizeBash, 2)
+          let data = inputFileToJSON(filePath)
+          config.CREATIONRETRY = Math.max(poolSizeBash * 5, data.maxY, data.maxX, 30)
+          console.time('evolution')
+          let results = evolution(filePath, config)
+          console.timeEnd('evolution')
+          jf.writeFileSync(resultsFileName, results)
+          console.log('Finished with file', file)
+        })
+        poolSizeBash++
+      }
       done()
     })
   })
