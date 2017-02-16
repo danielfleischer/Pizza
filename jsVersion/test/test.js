41b82b409284eb7b67b163c8d8f05f2036d4d4b9
thehobbit85 <thehobbit85@gmail.com>
2017-02-16 22:57:32 +0200

    small tests change

--- jsVersion/test/test.js
+++ jsVersion/test/test.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 const path = require('path')
 const resultsdir = path.join(__dirname, './results/')
 const inputFolder = path.join(__dirname, './inputs')
-const inputFileToJSON = require(path.join(__dirname, './../lib/inputFileToJSON'))
 
 let config = Object.assign({RUNS: 1}, process.env)
 
@@ -17,12 +16,11 @@ describe('testing on google samples', function () {
     fs.readdir(inputFolder, function (err, files) {
       if (err) throw err
       for (let i = 0; i < config.RUNS; i++) {
-        files.forEach(function (file) {
+        files.filter(file => file.split('.')[1] === 'in').forEach(function (file) {
           let filePath = path.join(inputFolder, file)
           let resultsFileName = path.join(resultsdir, file.split('.')[0] + '-' + (new Date()).getTime() + '.json')
           config.POPULATIONSIZE = Math.pow(poolSizeBash, 2)
-          let data = inputFileToJSON(filePath)
-          config.CREATIONRETRY = Math.max(poolSizeBash * 5, data.maxY, data.maxX, 30)
+          config.CREATIONRETRY = 1000
           console.time('evolution')
           let results = evolution(filePath, config)
           console.timeEnd('evolution')
