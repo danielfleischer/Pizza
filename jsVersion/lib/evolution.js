4ef67b1b202b4e3f506eb339c5322d19b4e0524a
thehobbit85 <thehobbit85@gmail.com>
2017-02-16 22:56:42 +0200

    Some timers

--- jsVersion/lib/evolution.js
+++ jsVersion/lib/evolution.js
@@ -14,39 +14,51 @@ module.exports = ({
   let primitives = evolutionPrimitives(matrix, L, H)
   let maxFitness = matrix.length * matrix[0].length
   let population = []
-
+  console.time('Creating Population')
   for (let i = 0; i < POPULATIONSIZE; i++) {
+    console.time('Creating Child number', i + 1)
     let child = primitives.createInstance(CREATIONRETRY)
     population.push(child)
+    console.timeEnd('Creating Child number', i + 1)
   }
-
+  console.timeEnd('Creating Population')
   if (verbose) results.initialPopulation = primitives.fit(population)
 
   for (let i = 0; i < GENERATIONS; i++) {
+    console.time('Generation Number: ' + (i + 1))
     let generation = { generation: i }
+
+    console.time('1. Select Fittest')
     let currentPopulation = primitives.fit(population)
     let topPopulationSize = Math.sqrt(POPULATIONSIZE)
     verbose && Object.assign(generation, { population: currentPopulation })
     let fittestChildren = currentPopulation.slice(0, topPopulationSize)
     let fittest = fittestChildren[0]
     generation.fittest = fittest
+    console.timeEnd('1. Select Fittest')
 
     results.generations.push(generation)
 
     if (fittest.fitness === maxFitness) break
     population = []
 
+    console.time('2. Mate')
     for (let j = 0; j < fittestChildren.length; j++) {
       for (let k = 0; k < fittestChildren.length; k++) {
         let child = primitives.mate(fittestChildren[j].solution, fittestChildren[k].solution, DRIFT)
         population.push(child)
       }
     }
+    console.timeEnd('2. Mate')
 
+    console.time('3. Mutate')
     population = population
-      .map(child =>
-        mutils.getRandomInt([1, 100]) < MUTATIONRATE ? primitives.mutate(child, MUTATIONRETRY) : child)
+      .map(child => mutils.getRandomInt([1, 100]) < MUTATIONRATE ? primitives.mutate(child, MUTATIONRETRY) : child)
+    console.timeEnd('3. Mutate')
+
+    console.timeEnd('Generation Number' + (i + 1))
   }
+
   let lastPopulation = primitives.fit(population)
   if (verbose) results.lastPopulation = lastPopulation
   results.fittest = lastPopulation[0]
